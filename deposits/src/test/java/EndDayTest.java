9fb0319194cfe08bb2572b0a6e5173df333acb2d
vpashnya <vpashnya@gmail.com>
2025-03-23 22:17:33 +0300

    Исправления по результатам CR

--- deposits/src/test/java/EndDayTest.java
+++ deposits/src/test/java/EndDayTest.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import ru.pvn.levelup.utils.EndOperDayUtils;
@@ -7,6 +8,7 @@ import ru.pvn.levelup.utils.EndOperDayUtils;
 public class EndDayTest {
     @Test
     @DisplayName("Завершения дня")
+    @Disabled
     public void endDatTest() {
         Assertions.assertDoesNotThrow(()->{
             EndOperDayUtils.runEndOperDay();
